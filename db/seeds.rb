d62cd973a810800b3c3f5f188bcbd20bef1402c7
Sy Rashid <sy.rashid.11@gmail.com>
2021-06-10 19:25:25 -0400

    FEAT: Add seeds for teachers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating teachers...'
+
+Teacher.create!({
+  name: "Sy",
+  campus: "Oslo"
+})
+Teacher.create!({
+  name: "George",
+  campus: "Melbourne"
+})
+Teacher.create!({
+  name: "Cassie",
+  campus: "Melbourne"
+})
+
+puts "Finished seeding #{Teacher.count} teachers!"
