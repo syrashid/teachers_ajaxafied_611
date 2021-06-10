9c4e777a1db4ac4a45a5810e253dc24054f2106a
Sy Rashid <sy.rashid.11@gmail.com>
2021-06-10 19:44:57 -0400

    FEAT: Add review function for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :teachers, only: [ :index, :show]
-  # Later, we'll put in reviews
+  resources :teachers, only: [ :index, :show] do
+    resources :reviews, only: :create
+  end
 end
